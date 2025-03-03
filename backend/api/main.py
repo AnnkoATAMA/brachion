428053271f2dda780dfedc215653b145d02d8dd7
AnnkoATAMA <annkopan0903@gmail.com>
2025-03-03 20:11:45 +0900

    ほんとごめん

--- backend/api/main.py
+++ backend/api/main.py
@@ -4,16 +4,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost",
-    "http://127.0.0.1",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -29,4 +22,4 @@ prefix = "/api"
 # # router足すときはここに追記してね
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True)
