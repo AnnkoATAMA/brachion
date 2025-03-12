4feea13dba16082a67e3c125fc89406fe4d6dd42
Kazuki <Nika42538@gmail.com>
2025-03-13 04:34:34 +0900

    Room nakamihenkou

--- frontend/src/components/room/Room.tsx
+++ frontend/src/components/room/Room.tsx
@@ -1,7 +1,7 @@
 import { use } from "react";
-import { Container, List, ListItem, ListItemText, Typography, Button } from "@mui/material";
-import { fetchRooms, deleteRoom, joinRoom } from "../../utils/roomApi";
-import {useNavigate} from "react-router-dom";
+import { Container, List,Typography,} from "@mui/material";
+import { fetchRooms,} from "../../utils/roomApi";
+import RoomListItem from "./RoomListItem.tsx";
 
 interface RoomType {
     id: number;
@@ -13,26 +13,6 @@ const roomListPromise = fetchRooms();
 
 const Room = () => {
     const rooms: RoomType[] = use(roomListPromise);
-    const navigate = useNavigate();
-
-    const handleDeleteRoom = async (roomId: number) => {
-        try {
-            await deleteRoom(roomId);
-            window.location.reload();
-        } catch (err) {
-            console.error("部屋の削除に失敗:", err);
-        }
-    };
-
-    const handleJoinRoom = async (roomId: number) => {
-        try {
-            await joinRoom(roomId);
-            console.log(`部屋 ${roomId} に参加しました`);
-            navigate(`/room/${roomId}`);
-        } catch (err) {
-            console.error("部屋の参加に失敗:", err);
-        }
-    };
 
     return (
         <Container sx={{ mt: 4 }}>
@@ -41,18 +21,7 @@ const Room = () => {
             </Typography>
             <List>
                 {rooms.map((room) => (
-                    <ListItem key={room.id} sx={{ display: "flex", justifyContent: "space-between" }}>
-                        <ListItemText
-                            primary={`部屋ID: ${room.id}`}
-                            secondary={`最大プレイヤー: ${room.max_players}, ゲームタイプ: ${room.game_type}`}
-                        />
-                        <Button variant="contained" color="error" onClick={() => handleDeleteRoom(room.id)}>
-                            削除
-                        </Button>
-                        <Button variant="contained" color="success" onClick={() => handleJoinRoom(room.id)}>
-                            参加
-                        </Button>
-                    </ListItem>
+                    <RoomListItem key={room.id} id={room.id} max_players={room.max_players} game_type={room.game_type}/>
                 ))}
             </List>
         </Container>
