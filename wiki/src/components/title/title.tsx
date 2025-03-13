17cc9223a68b17edf523727c93f5e90c040c0e2d
rglww8wsQI <kentarou9.60@icloud.com>
2025-03-13 22:05:05 +0900

    title component

---
+++ wiki/src/components/title/title.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+
+export const Title = component$(() => {
+    return (
+        <div class="title-container">
+            <h1>バイナリ麻雀・ルール解説</h1>
+        </div>
+    )
+   
+})
