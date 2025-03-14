5cda589ba5a53017e7268df9cae513cc62989b7c
rglww8wsQI <kentarou9.60@icloud.com>
2025-03-14 14:24:49 +0900

    麻雀の基本ルール作成

--- wiki/src/routes/index.tsx
+++ wiki/src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Title } from "../components/title/title";
+import { Rule } from "../components/rule/rule";
+import "../components/rule/style.css";
 import "../components/title/style.css";
 
 export default component$(() => {
   return (
-    <Title />
+    <>
+      <Title />
+      <Rule />
+    </> 
   );
 });
 
