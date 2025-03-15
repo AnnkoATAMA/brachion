745cb7a8fa08d3c5ca3b6a0025ac82fdd2f5df23
rglww8wsQI <kentarou9.60@icloud.com>
2025-03-15 21:37:25 +0900

    バイナリ検索

--- wiki/src/components/title/title.tsx
+++ wiki/src/components/title/title.tsx
@@ -1,10 +1,9 @@
 import { component$ } from "@builder.io/qwik";
 
 export const Title = component$(() => {
-    return (
-        <div class="title-container">
-            <h1>バイナリ麻雀・ルール解説</h1>
-        </div>
-    )
-   
-})
+  return (
+    <div class="title-container">
+      <h1>バイナリ麻雀・ルール解説</h1>
+    </div>
+  );
+});
--- wiki/src/routes/index.tsx
+++ wiki/src/routes/index.tsx
@@ -2,24 +2,27 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Title } from "../components/title/title";
 import { Rule } from "../components/rule/rule";
+import { Binary } from "../components/binary/binary";
 import "../components/rule/style.css";
 import "../components/title/style.css";
+import "../components/binary/style.css";
 
 export default component$(() => {
   return (
     <>
       <Title />
       <Rule />
-    </> 
+      <Binary />
+    </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "バイナリ麻雀wiki",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "バイナリ麻雀についての情報をまとめたwikiです。",
     },
   ],
 };
