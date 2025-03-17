b4a4049b15ec9ad5de35dcf5a02de0884c23941c
rglww8wsQI <kentarou9.60@icloud.com>
2025-03-17 11:58:57 +0900

    ws

--- backend/api/main.py
+++ backend/api/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import user
 from routers import room
+from routers import game
 
 app = FastAPI()
 
@@ -32,6 +33,7 @@ def health():
 prefix = "/api"
 app.include_router(user.user_router, prefix=prefix)
 app.include_router(room.room_router)
+app.include_router(game.game_router)
 
 
 if __name__ == "__main__":
