7826efe70909745b87ad8c8a7bc4c57c29b2084b
Kazuki <Nika42538@gmail.com>
2025-03-18 12:37:58 +0900

    GamePageの細かい調整,discardTiles,newDiscardTilesのリストと変数追加

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -7,10 +7,16 @@ import OpenDialogButton from "../components/mahjong/OpenDialogButton.tsx";
 
 const GamePage = () => {
     const [myHand] = useState(["110001 11100011 10000011 10011110 11100011 10000011 10110011 11100011 10000010 10111010","11101000 10110101 10100100 110101 11100011 10000010 10111101 11100011 10000011 10111100 11100011 10000010 10111010","11100111 10011001 10111101","3p","1m","2m","3m","1s","2s","3s","東","南","西","北",]);
-    const [hiddenHand,] = useState(Array(13).fill("裏")); // 裏向き牌の表示用
-    const [discardTiles,] = useState({
+    const [hiddenHand,] = useState(Array(13).fill("裏"));
+    const [newDiscardTiles] = useState<{ [key: string]: string }>({
+        自分: "1",
+        対面: "2",
+        上家: "4",
+        下家: "6",
+    });
+    const [discardTiles] = useState<{ [key: string]: string[] }>({
         自分: [],
-        対面: ["3s"],
+        対面: [],
         上家: [],
         下家: [],
     });
@@ -38,9 +44,9 @@ const GamePage = () => {
                 <Box className="toi-discard-area">
                     <OpenDialogButton buttonText="対面の捨て牌" discardTiles={discardTiles.対面} />
                     <Box className="discard-pile">
-                        {discardTiles.対面.map((tile, i) => (
-                            <Typography key={i}>{tile}</Typography>
-                        ))}
+
+                        <Typography >{newDiscardTiles.対面}</Typography>
+
                     </Box>
                 </Box>
 
@@ -60,11 +66,9 @@ const GamePage = () => {
                         <OpenDialogButton buttonText="上家の捨て牌" discardTiles={discardTiles.上家} />
 
                         <Grid container spacing={1}>
-                            {discardTiles.上家.map((tile, i) => (
-                                <Grid key={i}>
-                                    <Typography>{tile}</Typography>
-                                </Grid>
-                            ))}
+                            <Grid>
+                                <Typography>{newDiscardTiles.上家}</Typography>
+                            </Grid>
                         </Grid>
                     </Grid>
 
@@ -77,11 +81,9 @@ const GamePage = () => {
                     <Grid sx={{ width: "18%", border: "2px solid black", p: 4 }} className="ks-discard-area">
                         <OpenDialogButton buttonText="下家の捨て牌" discardTiles ={discardTiles.下家} />
                         <Grid container spacing={1}>
-                            {discardTiles.下家.map((tile, i) => (
-                                <Grid key={i}>
-                                    <Typography>{tile}</Typography>
-                                </Grid>
-                            ))}
+                            <Grid>
+                                <Typography>{newDiscardTiles.下家}</Typography>
+                            </Grid>
                         </Grid>
                     </Grid>
 
@@ -100,11 +102,9 @@ const GamePage = () => {
                 <Box className="my-discard-area">
                     <OpenDialogButton buttonText="自分の捨て牌" discardTiles={discardTiles.自分} />
                     <Box>
-                        {discardTiles.自分.map((tile, i) => (
-                            <Grid key={i}>
-                                <Typography>{tile}</Typography>
-                            </Grid>
-                        ))}
+                        <Grid>
+                            <Typography>{newDiscardTiles.自分}</Typography>
+                        </Grid>
                     </Box>
                 </Box>
 
